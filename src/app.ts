68ff279e88a018d7a40aa132415091a2ed7ca0e9
Nate Thompson <nthompson@pietech.com>
2017-09-05 16:53:36 -0400

    Aurelia CLI build and scaffolding
    Aurelia CLI build and scaffolding

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
+export class App {
+  public router: Router;
+
+  public configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'BLOCK NAME HERE';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: 'home/home', nav: true, title: 'Home' },
+      { route: 'link2', name: 'link2', moduleId: 'link2', nav: true, title: '[LINK 2]' },
+      { route: 'link3', name: 'link3', moduleId: 'link3', nav: true, title: '[LINK 3]' }
+    ]);
+
+    this.router = router;
+  }
+}
